cf45777e8f7beef282aeec99ace9071246d25db3
Dushime Aline Denise <alinedushdenyse@gmail.com>
2026-03-10 16:42:05 +0200

    simple successful message in Comfirmation page

---
+++ src/pages/ConfirmationPage.jsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom"
+
+export default function ConfirmationPage() {
+  const navigate = useNavigate()
+  return (
+    <div>
+      <h2>🎉 Registration Successful!</h2>
+      <p>Thank you! Your seat has been reserved.</p>
+      <button onClick={() => navigate("/")}>Back to Home</button>
+    </div>
+  )
+}
